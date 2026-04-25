ba8eb07572934a54761c6f5ee89e3fe2d24ea360
bruno-felipe-conte <Brunofelipe.conti@gmail.com>
2026-04-25 12:17:41 -0300

    chore: remove non-nexomente files from repo

--- inspect-graph.py
+++
@@ -1,8 +0,0 @@
-import json
-from collections import Counter
-
-with open('graphify-out/graph.json', 'rb') as f:
-    g = json.load(f)
-    
-print("Keys:", list(g.keys())[:5])
-print(f"Nodes sample: {g['nodes'][0] if g['nodes'] else 'empty'}")
